18d2803be2c3579db9c4588d9548706b43766289
jakemulley <jakemulley@users.noreply.github.com>
2021-12-06 17:37:50 +0000

    Configure Terraform

---
+++ terraform/locals.tf
@@ -0,0 +1,8 @@
+locals {
+  tags = {
+    business-unit = "Platforms"
+    is-production = true
+    owner         = "Operations Engineering: operations-engineering@digital.justice.gov.uk"
+    source-code   = "https://github.com/ministryofjustice/dns-iac"
+  }
+}
---
+++ terraform/versions.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "s3" {
+    bucket         = "dns-terraform-state20211206173317424200000001"
+    dynamodb_table = "dns-terraform-state-lock"
+    key            = "terraform.tfstate"
+    region         = "eu-west-2"
+    encrypt        = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-west-2"
+}
