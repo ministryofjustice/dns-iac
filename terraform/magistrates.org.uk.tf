4bd400e2578ba16ca00e3b11afb98859de025c32
Ben Ashton <ben.ashton@digital.justice.gov.uk>
2022-11-25 16:33:45 +0000

    Import missing hosted zones

---
+++ terraform/magistrates.org.uk.tf
@@ -0,0 +1,78 @@
+module "magistrates_org_uk_zone" {
+  source = "./modules/route53/zone"
+
+  name = "magistrates.org.uk"
+  tags = {
+    component = "None"
+  }
+}
+
+module "magistrates_org_uk_records" {
+  source = "./modules/route53/records"
+
+  zone_id = module.magistrates_org_uk_zone.zone_id
+
+  records = [
+    {
+      name = "magistrates.org.uk"
+      type = "A"
+      ttl  = 300
+      records = [
+        "52.30.196.9"
+      ]
+    },
+    {
+      name = "magistrates.org.uk"
+      type = "NS"
+      ttl  = 172800
+      records = [
+        "ns-1347.awsdns-40.org.",
+        "ns-2010.awsdns-59.co.uk.",
+        "ns-313.awsdns-39.com.",
+        "ns-935.awsdns-52.net."
+      ]
+    },
+    {
+      name = "magistrates.org.uk"
+      type = "SOA"
+      ttl  = 900
+      records = [
+        "ns-935.awsdns-52.net. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400"
+      ]
+    },
+    {
+      name = "magistrates.org.uk"
+      type = "MX"
+      ttl  = 120
+      records = [
+        "10 cluster2.gsi.messagelabs.com.",
+        "20 cluster2a.gsi.messagelabs.com."
+      ]
+    },
+    {
+      name = "mo.admin.magistrates.org.uk"
+      type = "A"
+      ttl  = 600
+      records = [
+        "83.151.216.178"
+      ]
+    },
+    {
+      name = "mo.rota.magistrates.org.uk"
+      type = "A"
+      ttl  = 600
+      records = [
+        "83.151.216.178"
+      ]
+    },
+    {
+      name = "www.magistrates.org.uk"
+      type = "A"
+      ttl  = 600
+      records = [
+        "52.30.196.9"
+      ]
+    },
+  ]
+}
+
---
+++ terraform/trackmycrime.net.tf
@@ -0,0 +1,57 @@
+module "trackmycrime_net_zone" {
+  source = "./modules/route53/zone"
+
+  name = "trackmycrime.net"
+  tags = {
+    component = "None"
+  }
+
+  description = "Defensive domain for https://trackmycrime.police.uk"
+}
+
+module "trackmycrime_net_records" {
+  source = "./modules/route53/records"
+
+  zone_id = module.trackmycrime_net_zone.zone_id
+
+  records = [
+    {
+      name = "trackmycrime.net"
+      type = "A"
+      ttl  = 300
+      records = [
+        "52.30.196.9"
+      ]
+    },
+
+    {
+      name = "trackmycrime.net"
+      type = "NS"
+      ttl  = 172800
+      records = [
+        "ns-1905.awsdns-46.co.uk.",
+        "ns-1301.awsdns-34.org.",
+        "ns-847.awsdns-41.net.",
+        "ns-342.awsdns-42.com."
+      ]
+    },
+    {
+      name = "trackmycrime.net"
+      type = "SOA"
+      ttl  = 900
+      records = [
+        "ns-1905.awsdns-46.co.uk. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400"
+      ]
+    },
+    {
+      name = "www.trackmycrime.net"
+      type = "A"
+      alias = {
+        zone_id                = module.trackmycrime_net_zone.zone_id
+        name                   = "trackmycrime.net."
+        evaluate_target_health = false
+      }
+    },
+  ]
+}
+
