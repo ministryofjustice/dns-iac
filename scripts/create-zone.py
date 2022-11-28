34719dd77b5e7ac696dd9d87837d98267887d56c
Ben Ashton <ben.ashton@digital.justice.gov.uk>
2022-11-28 15:29:52 +0000

    Initial code to export Hosted Zone and Records straight into Terraform

---
+++ scripts/create-zone.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from lib.HostedZone import HostedZone
+from jinja2 import Template
+
+# Open up template file
+with open('scripts/templates/hosted-zone.j2') as file_:
+    template = Template(file_.read())
+    
+    
+    
+
+ZONE_NAME = "PUT YOUR ZONE NAME HERE, IT PRINTS OUT TO CONSOLE"
+zone = HostedZone.createFromName(ZONE_NAME)
+print(template.render(
+        zone=zone.zone,
+        tags=zone.get_tags(),
+        records=zone.get_records()
+    ))
---
+++ scripts/lib/HostedZone.py
@@ -0,0 +1,72 @@
+import boto3
+
+
+class HostedZone:
+    """This class should only be used to read data from AWS Route53 - it is not built to impact changes to anything in Route53
+    """
+    
+    def __init__(self, id):
+        """Create a HostedZone object from the ID of an AWS Route53 Hosted Zone
+
+        Args:
+            id (string): ID of an AWS Route53 Hosted Zone
+        """
+        
+        self.client = boto3.client('route53')
+        self.id = id
+        self.zone = self.client.get_hosted_zone(
+            Id = id
+        )
+        return
+    
+    @classmethod
+    def createFromName(cls, name):
+        """Create a HostedZone object from the name of an AWS Route53 Hosted Zone
+
+        Args:
+            name (string): Name of an AWS Route53 Hosted Zone
+
+        Raises:
+            Exception: Route53 Hosted Zone cannot be found
+
+        Returns:
+            HostedZone: HostedZone object
+        """
+        
+        client = boto3.client('route53')
+        zones = client.list_hosted_zones_by_name(
+            DNSName = name,
+            MaxItems = '1'
+        )
+        
+        if not zones or len(zones['HostedZones']) == 0:
+            raise Exception(f"Could not find DNS zone {name}")
+        
+        return cls(zones['HostedZones'][0]['Id'].split('/')[2])
+       
+    def get_records(self):
+        """Returns Records in Hosted Zone owned by this object
+
+        Returns:
+            dict: Dict holding all records in Hosted Zone owned by this object
+        """
+        
+        return self.client.list_resource_record_sets(
+            HostedZoneId = self.id,
+            MaxItems = '250'
+        )['ResourceRecordSets']
+        
+    def get_tags(self):
+        """Returns tags for Hosted Zone owned by this object
+
+        Returns:
+            dict: Dict holding all tags in Hosted Zone owned by this object
+        """
+        return self.client.list_tags_for_resource(
+            ResourceType='hostedzone',
+            ResourceId=self.id
+        )['ResourceTagSet']['Tags']
+        
+        
+        
+    
